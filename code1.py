0b3e94a3bf2cf67e77bd4c6b37baa59b1275cb98
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-10-12 17:08:15 -0300

    Update code1.py

--- code1.py
+++ code1.py
@@ -5,14 +5,3 @@ def criaPasta(pasta):
     if os.path.exists(pasta)==False:
         os.makedirs(pasta)
 
-origem = os.getcwd()+"/pastaA/"
-pastaB = os.getcwd()+"/pastaB/"
-
-criaPasta(origem)
-criaPasta(pastaB)
-
-dir_tree={
-    "Imagens/":[".png", ".gif", ".jpeg", ".jpg"],
-    "Sons/":[".mp3", ".mpeg", ".wav"],
-    "Documentos/":[".pdf", ".doc", ".txt"]
-}
